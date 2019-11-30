e78e223f0705644129b1c5ac1a1831300686dded
bestpizzatoo <36733639+bestpizzatoo@users.noreply.github.com>
2019-11-30 19:50:02 +0100

    Update spotifyd.sh

--- spotify/rootfs/etc/cont-init.d/spotifyd.sh
+++ spotify/rootfs/etc/cont-init.d/spotifyd.sh
@@ -11,17 +11,17 @@ declare bitrate
 #bashio::config.require.username
 #bashio::config.require.password
 
-#if ! bashio::config.has_value 'name'; then
-#    bashio::log.fatal
-#    bashio::log.fatal "Add-on configuration is incomplete!"
-#    bashio::log.fatal
-#    bashio::log.fatal "The Spotify client needs to be identifiable with a name"
-#    bashio::log.fatal "and it seems you haven't configured one."
-#    bashio::log.fatal
-#    bashio::log.fatal "Please set the 'name' add-on option."
-#    bashio::log.fatal
-#    bashio::exit.nok
-#fi
+if ! bashio::config.has_value 'name'; then
+    bashio::log.fatal
+    bashio::log.fatal "Add-on configuration is incomplete!"
+    bashio::log.fatal
+    bashio::log.fatal "The Spotify client needs to be identifiable with a name"
+    bashio::log.fatal "and it seems you haven't configured one."
+    bashio::log.fatal
+    bashio::log.fatal "Please set the 'name' add-on option."
+    bashio::log.fatal
+    bashio::exit.nok
+fi
 
 username=$(bashio::config 'username')
 password=$(bashio::config 'password')
@@ -29,8 +29,8 @@ name=$(bashio::config 'name')
 bitrate=$(bashio::config 'bitrate')
 
 {
-    echo "username = \"${username}\""
-    echo "password = \"${password}\""
+    #echo "username = \"${username}\""
+    #echo "password = \"${password}\""
     echo "device_name = \"${name}\""
     echo "bitrate = ${bitrate}"
 } >> /etc/spotifyd.conf
